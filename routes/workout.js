0e9e4f78be4335f4c72c50bc8a8fb15b42fb3427
Ferdinand Ismael <ferdinandismael@ferdinands-mbp.lan>
2020-05-15 01:25:56 -0400

    updated workout with patch route

--- routes/workout.js
+++ routes/workout.js
@@ -87,6 +87,61 @@ router.post(
   }
 );
 
+// @route   PATCH /api/workouts/:id
+// @desc    Update a workout
+// @access  Private
+
+router.patch(
+  '/:id',
+  [
+    auth,
+    check('description', 'Description is required'),
+    check('caloriesBurned', 'caloriesBurned is required'),
+  ],
+  async (req, res) => {
+    // check for errors in request
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { description, caloriesBurned, exercises } = req.body;
+
+      const workoutFields = {
+        description,
+        caloriesBurned,
+        exercises,
+        user: req.user.id,
+      };
+
+      let workout = await Workout.findOne({ _id: req.params.id });
+
+      if (!workout) {
+        return res.status(400).json({ errors: [{ msg: 'Workout not found' }] });
+      }
+
+      // update profile
+      workout = await Workout.findByIdAndUpdate(
+        // filter / selection criteria
+        { _id: req.params.id },
+        // sets fields in model
+        { $set: workoutFields },
+        // applies formatting
+        { new: true }
+      );
+
+      await workout.save();
+
+      res.json(workout);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 // @route   Delete /api/workouts/:id
 // @desc    Delete a workout
 // @access  Public

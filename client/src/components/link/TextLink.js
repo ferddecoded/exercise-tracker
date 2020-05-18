7a639806b2aaf4f464367a1e624f6b723f7c6ba3
Ferdinand Ismael <ferdinandismael@ferdinands-mbp.lan>
2020-05-18 15:20:36 -0400

    update textlink

--- client/src/components/link/TextLink.js
+++ client/src/components/link/TextLink.js
@@ -1,6 +1,12 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-export const TextLink = styled.a`
+export const TextLink = styled(({ routerLink, children, ...rest }) => (
+  <a as={!routerLink ? 'a' : RouterLink} {...rest}>
+    {children}
+  </a>
+))`
   cursor: pointer;
   display: flex;
   align-items: center;

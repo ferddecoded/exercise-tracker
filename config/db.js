81014267acb3189190970c73fe4b14182b16d2b8
Ferdinand Ismael <ferdinandismael@ferdinands-mbp.lan>
2020-05-21 13:39:43 -0400

    update environment variables for deployment

--- config/db.js
+++ config/db.js
@@ -1,21 +1,20 @@
 const mongoose = require('mongoose');
 
-const db = process.env.MONGO_URI;
-
-console.log('mongo', db);
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
-      // Flag for using new URL string parser instead of current (deprecated) one
-      useNewUrlParser: true,
-      // Flag for using new Server Discovery and Monitoring engine instead of current (deprecated) one
-      useUnifiedTopology: true,
-      // If true, this connection will use createIndex() instead of ensureIndex() for automatic index builds via Model.init().
-      useCreateIndex: true,
-      // Set to false to make findOneAndUpdate() and findOneAndRemove() use native findOneAndUpdate() rather than findAndModify().
-      useFindAndModify: false,
-    });
+    await mongoose.connect(
+      `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0-d9wb9.mongodb.net/test?retryWrites=true&w=majority`,
+      {
+        // Flag for using new URL string parser instead of current (deprecated) one
+        useNewUrlParser: true,
+        // Flag for using new Server Discovery and Monitoring engine instead of current (deprecated) one
+        useUnifiedTopology: true,
+        // If true, this connection will use createIndex() instead of ensureIndex() for automatic index builds via Model.init().
+        useCreateIndex: true,
+        // Set to false to make findOneAndUpdate() and findOneAndRemove() use native findOneAndUpdate() rather than findAndModify().
+        useFindAndModify: false,
+      }
+    );
     console.log('MongoDB Connected');
   } catch (error) {
     console.error(error.message);
